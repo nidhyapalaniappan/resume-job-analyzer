842ba86d36febb723e9d9d126595dbc53d08f60e
nidhyapalaniappan <nidhyapalaniappan@gmail.com>
2025-05-24 17:45:43 -0400

    Update skill_suggester.py

--- utils/skill_suggester.py
+++ utils/skill_suggester.py
@@ -1,6 +1,12 @@
 
 import spacy
-nlp = spacy.load("en_core_web_sm")
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
+    nlp = spacy.load("en_core_web_sm")
+
 
 def extract_skills(text):
     doc = nlp(text)
